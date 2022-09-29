b19ff01e2da96e41b09fbeacdbeb95e4ac26bdc9
MiKalem898 <mikalem898@gmail.com>
2022-09-29 22:38:41 +0200

    v1.2 = sans conv. des vitesses

---
+++ classes/Color_converter.py
@@ -0,0 +1,13 @@
+from customtkinter import *
+from helpers import codes
+from PIL import Image
+
+
+class ImagesCompresser():
+    def __init__(self, canvas):
+        self.canvas = canvas
+
+
+
+    def valid(self):
+       pass
